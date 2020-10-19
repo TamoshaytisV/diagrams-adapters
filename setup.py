b926ac3e8d5322f15df0777c89a0578c5eb96836
TamoshaytisV <amd.vladas@gmail.com>
2020-10-19 09:20:38 +0300

    update setup.py url and classifiers

--- setup.py
+++ setup.py
@@ -9,7 +9,20 @@ setup(
         'Set of adapters to generate architecture diagrams '
         'from popular text file formats.'
     ),
-    version="1.0.0",
+    url='https://github.com/TamoshaytisV/diagrams-adapters',
+    version="1.0.1",
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+
+    ],
+    python_requires='>=3.6',
     packages=find_packages(),
     include_package_data=False,
     install_requires=[
